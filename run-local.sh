59ba2e8540940f1cd096957b54c0b82d51766558
Dmitry Terentyev <dmitry.yu.terentyev@gmail.com>
2024-08-04 19:21:08 -0700

    Fixed restart+rebuild

--- run-local.sh
+++ run-local.sh
@@ -1,1 +1,1 @@
-docker build -t cycling-weather . && docker run -p 1234:1234 -v ./src:/app/src cycling-weather
+docker stop cycling-weather || docker build -t cycling-weather . && docker run --name cycling-weather -p 1234:1234 -v ./src:/app/src cycling-weather
